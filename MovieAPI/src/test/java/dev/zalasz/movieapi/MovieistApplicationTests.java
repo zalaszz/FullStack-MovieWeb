38f51817fbd41b9b4d6345c7297e8ee1fee0418f
zalaszz <rickysilva2002@hotmail.com>
2023-09-11 04:44:21 +0100

    First Commit

---
+++ MovieAPI/src/test/java/dev/zalasz/movieapi/MovieistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zalasz.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class movieapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
